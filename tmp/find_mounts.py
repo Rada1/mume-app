259f325e1f4424995326f7777987adf89f9dcc80
Rada1 <pwetzel42@gmail.com>
2026-03-19 12:51:34 -0500

    fixes to colors

--- tmp/find_mounts.py
+++
@@ -1,8 +0,0 @@
-import json
-filepath = r'c:\Users\pwetz\Downloads\mume app\src\constants\mastersettings.json'
-with open(filepath, 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-for b in data.get('buttons', []):
-    if b.get('setId') == 'inline-mounts':
-        print(json.dumps(b, indent=2))
